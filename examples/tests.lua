ab81d705ad1f3c478c5aebd8bf2479fb7dd29dc6
Luke Drummond <lukedrummond@gmail.com>
2014-05-16 05:34:43 +0100

    implemented fixtures, callable tables, expected failures

--- examples/tests.lua
+++ examples/tests.lua
@@ -28,11 +28,26 @@ passing  = TestingUnit{
     test_false = function(self) self:assert_false(false) end,
     test_true = function(self) self:assert_true(true) end,
     test_raises = function(self) self:assert_raises(function() return 0 / '' end) end,
-    test_in_string = function(self) self:assert_in('darkness ', 'Hello darkness my old friend') end,
-    test_in_table = function(self) self:assert_in(9, {1,2,3,4,5,6,7,8,9, k=function()end}) end,
+    test_in_string = function(self) self:assert_in('Hello darkness my old friend', 'darkness ' ) end,
+    test_in_table = function(self) self:assert_in({1,2,3,4,5,6,7,8,9, k=function()end},9 ) end,
     test_calls = function(self) self:assert_calls(mycaller, mycallee, {4, 5, 6}) end,
     test_truthy = function(self) self:assert_truthy(0) end,
     test_nil = function(self) self:assert_nil() end,
     test_match = function(self) self:assert_match("123", '[0-9]') end
 }
 
+expected = TestingUnit{
+    fixtures = {
+        test_fixtures_expected_failure={{1,2,3}, {4,5,6}, {7,8,9}},
+        test_table_call = {{1, 'hello'}}
+    },
+    test_false_expected_failure = function(self) self:assert_false(0) end,
+    test_equal_expected_failure = function(self) self:assert_equal(8, 'nope') end,
+    test_another_equal_expected_failure = function(self) self:assert_equal(9, 7) end,
+    test_in_string_expected_failure = function(self) self:assert_in('darkness ', 'morning sunshine!') end,
+    test_in_table_expected_failure = function(self) self:assert_in('darkness', {'hello', 9, k=function()end}) end,
+    test_calls_expected_failure = function(self) self:assert_calls(mycaller, myotherfunc, {4, 5, 6}) end,
+    test_match_expected_failure = function(self) self:assert_match("This string isn't a substring", "of this one") end,
+    test_fixtures_expected_failure = function(self, a,b,c)self:assert_equal(a,b)end,
+--    test_table_call = setmetatable({}, {__call=function(self, test_unit,b,c)print(a,b,c)end})
+}
--- src/testingunit.lua
+++ src/testingunit.lua
@@ -6,15 +6,23 @@ TestingUnit = setmetatable({},
         local test_table = {
             --this table value should be considered immutable as it is used for table discovery
             is_testing_unit = true,
-            failures = {},
-            errors = {},
-            expected_failures = {},
+            
             fixtures = {},
-                
+            
+            _assertion_failure = function(self, ...)
+                error("override me", 4)
+            end,
+            
             assert_raises = function(self, func, args)
-                success, result =  pcall(func, unpack(args))
+                --[[ given a callable ``func`` and table of arguments ``args``,
+                assert that ``func(unpack(args))`` throws an error.
+                    
+                ]]
+                local args = args or {}
+                success, result = pcall(func, unpack(args))
+                --we don't want success as this is assert_raises()
                 if success then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=result, 
                         func=func, 
                         args=args,
@@ -23,9 +31,10 @@ TestingUnit = setmetatable({},
                     }
                 end
             end,
+            
             assert_equal = function(self, a, b)
                 if a ~= b then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("assert_equal failed: %s ~= %s", a , b), 
                         func=func, 
                         args={a, b}
@@ -34,7 +43,7 @@ TestingUnit = setmetatable({},
             end,
             assert_truthy = function(self, x)
                 if not x then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("assert_truthy failed: '%s' is not truthy", x), 
                         info=debug.getinfo(2),
                         args={x}
@@ -43,7 +52,7 @@ TestingUnit = setmetatable({},
             end, 
             assert_true = function(self, x)
                 if x ~= true then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("assert_true failed:'%s' ~= true", x), 
                         info=debug.getinfo(2),
                         args={x}
@@ -52,7 +61,7 @@ TestingUnit = setmetatable({},
             end,
             assert_false = function(self, x)
                 if x ~= false then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("assert_false failed: '%s' ~= false", x), 
                         info=debug.getinfo(2),
                         args={x}
@@ -61,7 +70,7 @@ TestingUnit = setmetatable({},
             end,
             assert_match = function(self, s, exp)
                 if not string.match(s, exp) then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("'%s' does not match '%s'", s, exp), 
                         info=debug.getinfo(2),
                         args={s, exp}
@@ -70,7 +79,7 @@ TestingUnit = setmetatable({},
             end,
             assert_in = function(self, haystack, needle)
                 if type(haystack) ~= 'string' and type(haystack) ~= 'table' then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("type('%s') is not a container type",  haystack), 
                         info=debug.getinfo(2),
                         args={needle=needle, haystack=haystack}
@@ -79,7 +88,7 @@ TestingUnit = setmetatable({},
                 end
                 if type(haystack) == 'string' then
                     if type(needle) ~= 'string' then
-                        self.failures[#self.failures + 1] = {
+                        self:_assertion_failure{
                             err=string.format("'%s' cannot be a substring of '%s'", needle, haystack), 
                             info=debug.getinfo(2)
                         }
@@ -87,7 +96,7 @@ TestingUnit = setmetatable({},
                     end
                     local start, finish = string.find(haystack, needle)
                     if start == nil then
-                        self.failures[#self.failures + 1] = {
+                        self:_assertion_failure{
                             err=string.format("'%s' not found in '%s'", needle, haystack),  
                             info=debug.getinfo(2)
                         }
@@ -102,7 +111,7 @@ TestingUnit = setmetatable({},
                         end
                     end
                     if not in_table then
-                        self.failures[#self.failures + 1] = {
+                        self:_assertion_failure{
                             err=string.format("'%s' not found in '%s'", needle, haystack),  
                             info=debug.getinfo(2)
                         }
@@ -111,7 +120,7 @@ TestingUnit = setmetatable({},
             end,
             assert_nil = function(self, x)
                 if x ~= nil then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format("'%s' not nil", x),  
                         info=debug.getinfo(2)
                     }
@@ -141,10 +150,12 @@ TestingUnit = setmetatable({},
                     end
                 end
                 debug.sethook(trace_hook, 'c')
+                    --pcall is required because errors prevent reporting / cancelling the debug hook
+                    
                     pcall(caller,args)
                 debug.sethook()
                 if not was_called then
-                    self.failures[#self.failures + 1] = {
+                    self:_assertion_failure{
                         err=string.format(
                             "assert_calls failure:'%s' not called by '%s with args(%s)'", 
                             getname(callee), 
@@ -188,7 +199,9 @@ function enumerate_files(dirs)
     return all_files
 end
 
+
 function load_test_files(dirs)
+    --discover all lua test scripts in the given directories and protected load them
     local tests = {}
     for _, file in ipairs(enumerate_files(dirs)) do
         if string.match(file, '^test.*.lua$') then
@@ -201,84 +214,161 @@ function load_test_files(dirs)
     end
 end
 
+function get_fixtures_for_member_name(t, member_name)
+    if not t.fixtures[member_name] then return {{}} end
+    return t.fixtures[member_name]
+end
+
 function runtests()
-    local tests = {}
-    local tests_run = 0
-    local tests_failed = 0
-    local tests_expected_failed = 0
-    local test_errors = 0
+    local all_test_units = {}
+    local n_tests_run = 0
+    local n_tests_failed = 0
+    local n_tests_passed = 0
+    local n_tests_expected_failed = 0
+    local n_test_errors = 0
     local start_time = 0
     local stop_time = 0
-    --discover all lua test scripts in the current directory and protected load them
-    
-    
+    local failures = {}
+    local errors = {}    
+    local expected_failures = {}
     --[[iterate the global registry for tables with key ``is_testing_unit == true `` 
     and add that table to the list of unit test tables.
     ]]
     for k, v in pairs(_G) do
         if type(v) == 'table' then
             if v['is_testing_unit'] == true then
-                tests[#tests +1] = v
+                all_test_units[#all_test_units +1] = v
             end
         end
     end
     
 
-    local function _runtests(t)
+    local function _run_test_unit(t)
         --[[ Search each identified unit test table for member functions named ``test*``
         and execute each one within pcall, identifying and counting failures, 
         expected failures, and execution errors.
         ]]
+        local is_expected_failure
+        local last_assertion
+        
+        --We inject the following function into the test table so we can reference ``assertion_triggered``
+        local function _assertion_failure(self, ...)
+            last_assertion = {...}
+--            if self._is_expected_failure then
+--                self.expected_failures[#self.expected_failures + 1] = {...}
+--                return
+--            end
+--            self.failures[#self.failures + 1] = {...}
+        end
+        
+        t._assertion_failure = _assertion_failure
+        
+        --iterate all callables and call with any supplied fixtures
         for k, v in pairs(t) do
-            if type(v) == 'function' then
-                if  string.match(k, '^test') then
-                    if t.setup then t:setup() end
-                        local success, ret = pcall(v,t)
-                    if not success then 
-                        t.errors[#t.errors +1] = ret
+            if type(v) == 'function' or (type(v) == 'table' and getmetatable(v) ~= nil and type(getmetatable(v)['__call']) == 'function') then
+                local func_name, callable = k, v
+                if string.match(string.lower(func_name), '^test') then
+                    for _, fixture in ipairs(get_fixtures_for_member_name(t, func_name)) do
+                        --[[
+                            Expected failures are indicated by naming convention 
+                            rather than decorators or similar conventions used in other languges.
+                            The following member functions will be treated as expected failures:
+                                test_myfunction_expected_fail'
+                                'TestExpectedFailure'
+                                'test_myfunctionExpected_failREALLYUGLY'
+                            Basically any sane name beginning with 'test' having the 
+                            substrings, 'expected' and 'fail' following.
+                            See pattern below for details.
+                            
+                        ]]
+                        is_expected_failure = false
+                        last_assertion = nil
+                        if string.match(string.lower(func_name), '^test[%a%d_]*expected[%a%d_]*fail') then
+                            --[[The expected failure handling is a hack relying on 
+                            the table member variable ``_is_expected_failure`` as semi-global state,
+                            but it significantly simplifies the assert_*  family of functions.
+                            ]]
+                        is_expected_failure = true
+                        end
+                        
+                        
+                        --execute the test with data
+                        if t.setup then t:setup() end
+                            local success, ret
+                            if #fixture > 0 then
+                                success, ret = pcall(callable, t, unpack(fixture))
+                            else
+                                success, ret = pcall(callable, t)
+                            end
+                            if not success then 
+                                errors[#errors +1] = {name=func_name, err=ret, args=fixture}
+                                break
+                            end
+                            n_tests_run = n_tests_run + 1
+                        if t.teardown then t:teardown() end
+                        
+                        if is_expected_failure then
+                            if not last_assertion then
+                                n_tests_failed = n_tests_failed + 1
+                                failures[#failures +1] = {
+                                    err=string.format("%s: did not fail as expected", func_name), 
+                                    info=debug.getinfo(callable),
+                                    args=#fixture > 0 and fixture or nil
+                                }
+                            else
+                                n_tests_expected_failed = n_tests_expected_failed +1
+                                n_tests_passed = n_tests_passed +1
+                            end
+                        else
+                            if last_assertion then
+                                n_tests_failed = n_tests_failed + 1
+                                failures[#failures +1] = last_assertion
+                            else
+                                 n_tests_passed = n_tests_passed +1
+                            end
+                        end
                     end
-                    tests_run = tests_run + 1
-                    if t.teardown then t:teardown() end
                 end
             end
         end
     end
     
+    --actually run all the tests
     start_time = os.time()
-        --actually run all the tests
-        for _, v in pairs(tests) do
-            _runtests(v)
+        for _, v in pairs(all_test_units) do
+            _run_test_unit(v)
         end
     stop_time = os.time()
     
-    local function _print_results(t) 
-        for _, f in ipairs(t.failures) do
-            tests_failed = tests_failed + 1
+    local function _print_results(t)
+        for _, f in ipairs(failures) do
             print("================================")
             print("FAILURE:")
-            for k, v in pairs(f) do
+            for k, v in pairs(f[1]) do
                 print(k, v)
             end
             print("--------------------------------")
         end
-        for _, e in ipairs(t.errors) do
-            test_errors = test_errors + 1
+        for _, e in ipairs(errors) do
+            n_test_errors = n_test_errors + 1
             print("================================")
             print("ERROR:execution failure")
             print(e)
             print("--------------------------------")
         end
     end
-    for _, v in pairs(tests) do
+    for _, v in pairs(all_test_units) do
         _print_results(v)
     end
     
     print(string.format([[Ran %s tests in %s seconds.
-        %s failures, %s expected failures, %s errors]],
-        tests_run, 
+        %s failures, %s expected failures, %s errors, %s passed]],
+        n_tests_run, 
         os.difftime(stop_time, start_time), 
-        tests_failed, 
-        tests_expected_failed, test_errors)
+        n_tests_failed, 
+        n_tests_expected_failed, 
+        n_test_errors,
+        n_tests_passed)
     )
 end
 
